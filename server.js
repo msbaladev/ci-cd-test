d424229f24770f25eb52eba4d7709bad260fa8e7
msbaladev <balamurugan@inesssolutions.com>
2024-12-10 10:29:01 +0530

    pipe

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const app = express();
 
 app.get("/", (req, res) => {
   res.status(200).json({
-    message: "horray! how're you buddy boooo!jjjj!!!!!!",
+    message: "horray! how're you buddy!!!",
   });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5500;
 app.listen(PORT, () => console.log(`hello welcome ${PORT}`));
